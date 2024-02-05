13d5ffb6d5761d9f77918141d7f4e72384d2687d
kumdo0327 <kumdo0327@naver.com>
2024-02-05 19:25:17 +0900

    update

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@ int main(int argc, char *argv[])
     PAFL::Pipeline(argc, argv).run();
     return 0;
 }
+
 /*
     PAFL::CppTokenTree tree(fs::path("sample/temp.cpp"), std::make_shared<PAFL::TokenTree::Matcher>());
     tree.log("token.txt");
