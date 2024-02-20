1b4fd2910f760180686f49d93ef8ba8bad0db392
kumdo0327 <kumdo0327@naver.com>
2024-02-20 21:00:26 +0900

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,14 @@
 #include "pipeline.h"
 
-int run(int argc, char* argv[])                                     { PAFL::Pipeline(argc, argv).run(); return 0; }
-int makeMatrix(int argc, char *argv[], const fs::path& path)        { PAFL::Pipeline(argc, argv).makeMatrix("/home/donguk/aeneas/manybugs"); return 0; }
-int testTokenTreeCpp(const fs::path& path)                          { PAFL::TokenTreeCpp tree(path, std::make_shared<PAFL::TokenTree::Matcher>()); tree.log("__log.txt"); return 0; }
-int testTokenTreePy(const fs::path& path)                           { PAFL::TokenTreePy tree(path, std::make_shared<PAFL::TokenTree::Matcher>(), "pytree.py"); tree.log("__log.txt"); return 0; }
+int run(int argc, char* argv[])             { PAFL::Pipeline(argc, argv).run(); return 0; }
+int makeMatrix(int argc, char *argv[])      { PAFL::Pipeline(argc, argv).makeMatrix("/home/donguk/aeneas/manybugs"); return 0; }
+int testTokenTreeCpp(const fs::path& path)  { PAFL::TokenTreeCpp tree(path, std::make_shared<PAFL::TokenTree::Matcher>()); tree.log("__log.txt"); return 0; }
+int testTokenTreePy(const fs::path& path)   { PAFL::TokenTreePy tree(path, std::make_shared<PAFL::TokenTree::Matcher>(), "pytree.py"); tree.log("__log.txt"); return 0; }
 
 
 
 int main(int argc, char *argv[])
 {
+    return makeMatrix(argc, argv)
     return run(argc, argv);
 }
