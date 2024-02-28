4584fd2053373ef4516a52ec0384d67a86a2baa3
kumdo0327 <kumdo0327@naver.com>
2024-02-28 16:37:47 +0900

    update

--- src/pipeline.cpp
+++ src/pipeline.cpp
@@ -1,5 +1,5 @@
 #include "pipeline.h"
-
+#include <iostream>
 namespace PAFL
 {
 Pipeline::Pipeline(int argc, char *argv[]) :
@@ -147,13 +147,17 @@ void Pipeline::localizeWithPAFL(FLModel& model, time_vector& time_vec)
 {
     _timer.restart();
 
+        std::cout << "normalizing\n";
         _method->setBaseSus(_suite, _ui.getProject(), std::to_string(_ui.getVersion(_iter)), std::to_string(_iter + 1));
         _normalizer->normalize(_suite);
 
         // Make token tree
         TokenTree::Vector tkt_vector(_suite->maxIndex());
-        for (index_t idx = 0; idx != _suite->maxIndex(); idx++)
+        for (index_t idx = 0; idx != _suite->maxIndex(); idx++) {
+            
+            std::cout << _ui.getFilePath(_iter, _suite->getFileFromIndex(idx)) << '\n';
             (this->*_builder)(tkt_vector[idx], _ui.getFilePath(_iter, _suite->getFileFromIndex(idx)));
+        }
 
         // New sus of FL Model
         std::cout << '\n' << _ui.getProject() << " : " << _method->getName() << "-pafl\n";
