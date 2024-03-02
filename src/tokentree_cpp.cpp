a93e62037a3cf66d411d8f10417f9a5c27d834d4
kumdo0327 <kumdo0327@naver.com>
2024-03-02 11:52:26 +0900

    update

--- src/tokentree_cpp.cpp
+++ src/tokentree_cpp.cpp
@@ -12,7 +12,7 @@ TokenTreeCpp::TokenTreeCpp(const std::filesystem::path& src_file, std::shared_pt
 
     { // From token list to token tree
         auto raw_stream(_getRawStream(src_file, matcher));
-        std::cout << src_file << '\n';
+        std::cout << src_file << std::endl;
         for (auto& token : raw_stream) {
             
             Token* tok = &token;
