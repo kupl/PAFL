13fc3eecae1d1a02b3de61d509a966e86544b401
kumdo0327 <kumdo0327@naver.com>
2024-03-04 10:57:00 +0900

    update

--- src/pipeline.cpp
+++ src/pipeline.cpp
@@ -149,13 +149,10 @@ void Pipeline::localizeWithPAFL(FLModel& model, time_vector& time_vec)
 {
     _timer.restart();
 
-        std::cout << "Tokenizing ...";
         // Make token tree
         TokenTree::Vector tkt_vector(_suite->maxIndex());
         for (index_t idx = 0; idx != _suite->maxIndex(); idx++)
-            (this->*_builder)(tkt_vector[idx], _ui.getFilePath(_iter, _suite->getFileFromIndex(idx)));
-        std::cout << " done";
-        
+            (this->*_builder)(tkt_vector[idx], _ui.getFilePath(_iter, _suite->getFileFromIndex(idx)));     
 
         // New sus of FL Model
         _method->setBaseSus(_suite, _ui.getProject(), std::to_string(_ui.getVersion(_iter)), std::to_string(_iter + 1));
@@ -185,7 +182,7 @@ void Pipeline::localizeWithPAFL(FLModel& model, time_vector& time_vec)
         // Destroy token tree
         for (auto ptr : tkt_vector)
             delete ptr;
-        std::cout << " done\n";
+        std::cout << " done\n\n";
 
     time_vec[_iter + 1] += _timer.stop();
 }
