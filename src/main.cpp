0d907907f239c72e3b8f34f98ad7855b5ba5ef6d
kumdo0327 <kumdo0327@naver.com>
2024-03-08 11:17:27 +0900

    update

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,5 @@ int testTokenTreePy(const fs::path& path)   { PAFL::TokenTreePy tree(path, std::
 
 int main(int argc, char *argv[])
 {
-    return makeMatrix(argc, argv);
     return run(argc, argv);
 }
